4646f5e0eee6e59804b33b099150ea5818a98af1
Dev Chawla <devchawla.04@gmail.com>
2026-04-13 21:05:45 +0530

    projects added

--- src/staticdata/projects.tsx
+++ src/staticdata/projects.tsx
@@ -21,8 +21,19 @@ const projects: Project[] = [
     skills: ["REACT JS", "TypeScript", "Tailwind", "Node JS", "MongoDB", "Express JS"],
     status: "running",
   },
-    {
+   {
     id: 2,
+    image: "/ai-meal.png",
+    title: "AI Meal Generator",
+    description:
+      "An AI Meal Generator that allows users to generate meals based on their preferences.",
+    linkURL: "https://ai-meal-maker.vercel.app/",
+    githubURL: "https://github.com/devchawla04/AI-Meal-Maker",
+    skills: ["REACT JS", "TypeScript", "Tailwind", "Node JS", "PostgressSQL", "Express JS"],
+    status: "running",
+  },
+    {
+    id: 3,
     image: "/netflix-clone.png",
     title: "Netflix Clone",
     description:
@@ -32,6 +43,18 @@ const projects: Project[] = [
     skills: ["REACT JS", "TypeScript", "Tailwind"],
     status: "running",
   },
+   {
+    id: 3,
+    image: "/google-clone.png",
+    title: "Google Clone",
+    description:
+      "A Google Clone that allows users to search and browse the web.",
+    linkURL: "https://dev-chawla-google-clone-project.netlify.app/",
+    githubURL: "https://github.com/devchawla04/google-clone",
+    skills: ["REACT JS", "TypeScript", "Tailwind",],
+    status: "running",
+  },
+  
 
 
 ];
